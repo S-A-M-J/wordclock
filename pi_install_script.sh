f3d82550dba0ac9c382af949b71f63cb962d48ad
S-A-M-J <60389168+S-A-M-J@users.noreply.github.com>
2022-09-13 16:55:22 +0200

    Update pi_install_script.sh

--- pi_install_script.sh
+++ pi_install_script.sh
@@ -27,6 +27,8 @@ sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 898
 
 echo "downloading github files..."
 cd
+sudo mkdir wordclock
+cd
 curl \-H 'Authorization: Bearer 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' \-H 'Accept: application/vnd.github.v3.raw' \ -L https://api.github.com/repos/S-A-M-J/wordclock/contents/pull_update_script.sh >wordclock/pull-update.sh
 
 echo "creating hotspot and wlan services"
