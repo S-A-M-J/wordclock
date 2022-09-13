dd950e53032b945ad43ec0fa7888608401ca48cf
S-A-M-J <60389168+S-A-M-J@users.noreply.github.com>
2022-09-13 17:05:28 +0200

    Update pull_update_script.sh

--- pull_update_script.sh
+++ pull_update_script.sh
@@ -5,7 +5,7 @@ echo "deleting old files if existent"
 if [ -d wordclock]; then
 	sudo rm -r wordclock
 fi
-sudo mkdir wordclock
+mkdir -m 777 wordclock
 echo "Updating python files from github..."
 echo "downloading new files..."
 curl \-H 'Authorization: Bearer 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' \-H 'Accept: application/vnd.github.v3.raw' \ -L https://api.github.com/repos/S-A-M-J/wordclock/contents/changeToWifi.sh >wordclock/changeToWifi.sh
