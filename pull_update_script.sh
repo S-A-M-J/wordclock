16b0e44546138209e24742cf56c4be3bce270702
S-A-M-J <60389168+S-A-M-J@users.noreply.github.com>
2022-09-13 18:21:48 +0200

    Update pull_update_script.sh

--- pull_update_script.sh
+++ pull_update_script.sh
@@ -15,13 +15,13 @@ curl \-H 'Authorization: Bearer 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' \-H 'A
 
 cd
 echo "downloading flow file..."
-cd ~/.node-red
+cd .node-red
 sudo rm flows.json
 cd
 curl \-H 'Authorization: Bearer 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' \-H 'Accept: application/vnd.github.v3.raw' \ -L https://api.github.com/repos/S-A-M-J/wordclock/contents/node-red-flows-wordclock.json >flows.json
 
 sudo touch flows.json
-sudo mv flows.json ~/.node-red/flows.json
+sudo mv flows.json .node-red/flows.json
 cd 
 
 node-red-restart
